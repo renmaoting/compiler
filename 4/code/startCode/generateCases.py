14ed4af6998942984dc1a1c35ca8f554d48bf4b2
renmaoting <rmtustc@gmail.com>
2016-10-27 16:26:15 -0400

    asg4

---
+++ 4/code/startCode/generateCases.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import os, sys
+import random 
+
+def generateCases():
+    num = random.randint(20, 30)
+    for i in range(num):
+        exp = str(random.randint(-15, 15))
+        cnt = 1
+        while 1:
+            cnt += 1;
+            if random.randint(0, 100) > 80 or cnt > 7:
+                break;
+            op = random.randint(0, 7)
+            val = random.randint(0, 15)
+            if op == 0: exp += '+'
+            elif op == 1: exp += '-'
+            elif op == 2: exp += '*'
+            elif op == 3: exp += '/'
+            elif op == 4: exp += '%'
+            elif op == 5: exp += '**'
+            else: exp += '//'
+            exp += str(val)
+        ans = 0
+        try:
+            ans = eval(exp)
+        except ValueError:
+            pass
+        
+        ansexp = 'print ' + exp+'\n';
+        fp = open('cases/'+str(i)+'.py', 'w');
+        fp.write(ansexp)
+        fp.close()
+        fp = open('cases/'+str(i)+'.out', 'w');
+        fp.write(str(ans)+'\n')
+        fp.close()
+
+generateCases()
+
